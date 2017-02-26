51d2c0a6a45d3640f91088916497c10d0fab4c91
skimb <skbsandin@gmail.com>
2017-02-26 17:35:06 +0100

    test ny inl

--- script/inl14.jsx
+++ script/inl14.jsx
@@ -13,8 +13,8 @@ class Ordlista extends React.Component {
   render() {
     let key = 0;
     const nyLista = this.props.lista.map(
-      obj => (<li key={obj.namn + obj.antal}>{obj.namn} 
-      ({obj.antal} st)</li>)
+      obj => (<li key={obj.word + obj.translation}>{obj.word} 
+      ( = {obj.translation})</li>)
     );
     return (<ol>{nyLista}</ol>);
   }
