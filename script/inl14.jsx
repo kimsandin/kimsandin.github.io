3d5c1c7e20cd9af49c60880c080660299c81050d
skimb <skbsandin@gmail.com>
2017-02-26 17:30:32 +0100

    test ny inl

---
+++ script/inl14.jsx
@@ -0,0 +1,27 @@
+const lista = [
+  {word: 'Häst', translation: 'Horse'},
+  {word: 'Travsele', translation: 'Trotting Harness'},
+  {word: 'Brösta', translation: 'Breast Collar'},
+  {word: 'Huvudlag', translation: 'Bridle'},
+  {word: 'Bett', translation: 'Bit'},
+  {word: 'Uppkäk', translation: 'Overcheck'},
+  {word: 'Tömmar', translation: 'Reins'},
+  {word: 'Bukgjord', translation: 'Girth'}
+];
+
+class Ordlista extends React.Component {
+  render() {
+    let key = 0;
+    const nyLista = this.props.lista.map(
+      obj => (<li key={obj.namn + obj.antal}>{obj.namn} 
+      ({obj.antal} st)</li>)
+    );
+    return (<ol>{nyLista}</ol>);
+  }
+}
+
+
+ReactDOM.render(
+  <Ordlista lista={lista} />,
+  document.getElementById('react-app')
+);
